b11a11986c4d98fe54166c38f0519e2c14ebfd75
Chetan Singh <chetan.singh@webeesocial.com>
2024-07-30 17:48:03 +0530

    added common folder

---
+++ resources/views/front/common/head.blade.php
@@ -0,0 +1,54 @@
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="stylesheet" href="{{ asset('front/css/bootstrap.css') }}">
+<link rel="stylesheet" href="{{ asset('front/css/viewer.css') }}">
+<link rel="stylesheet" href="{{ asset('front/css/style.css') }}">
+<link rel="stylesheet" href="{{ asset('front/css/datastyle.css') }}" />
+<link rel="stylesheet" href="{{ asset('front/css/docSlider.css') }}" />
+<link href="{{ asset('front/css/jquery.bxslider.css') }}" rel="stylesheet">
+<link href="{{ asset('front/css/owl.theme.css') }}" rel="stylesheet">
+<link href="{{ asset('front/css/owl.carousel.min.css') }}" rel="stylesheet">
+<link href="{{ asset('front/css/font-awesome.css') }}" rel="stylesheet">
+<link href="{{ asset('front/css/viewbox.css') }}" rel="stylesheet">
+<link href="{{ asset('front/css/colorbox.css') }}" rel="stylesheet">
+<link href="{{ asset('front/css/animate.css') }}" rel="stylesheet">
+<link href="{{ asset('front/css/fadeAnimation.css') }}" rel="stylesheet">
+<link href="{{ asset('front/css/responsive.css') }}" rel="stylesheet">
+<link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.0.0/animate.min.css"/>
+<!-- <link href="https://daneden.github.io/animate.css/animate.min.css" rel="styleSheet"/> -->
+<link href="https://fonts.googleapis.com/css2?family=Muli:ital,wght@0,200;0,300;0,400;0,500;0,577;0,600;0,700;0,800;0,900;1,200;1,300;1,400;1,500;1,577;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css2?family=Hind:wght@300;400;500;600;700&display=swap" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css2?family=Alex+Brush&display=swap" rel="stylesheet">
+
+
+
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+<script src="{{ asset('front/js/jquery.bxslider.min.js') }}"></script>
+<script src="{{ asset('front/js/viewer.js') }}"></script>
+<script src="{{ asset('front/js/main.js') }}"></script>
+<script src="{{ asset('front/js/owl.carousel.js') }}"></script>
+<script src="{{ asset('front/js/bootstrap.js') }}"></script>
+<script src="{{ asset('front/js/jquery.viewbox.min.js') }}"></script>
+<script src="{{ asset('front/js/jquery.colorbox.js') }}"></script>
+<script src="{{ asset('front/js/animations.js') }}"></script>
+<script src="{{ asset('front/js/animate-content.js') }}"></script>
+<script src="{{ asset('front/js/script.js') }}"></script>
+<script src="{{ asset('front/js/jquery.counterup.min.js') }}"></script>
+<script src="{{ asset('front/js/waypoints.min.js') }}"></script> 
+<script src="{{ asset('front/js/jquery.liMarquee.js') }}"></script>
+<script src="{{ asset('front/js/jquery.slimscroll.min.js') }}"></script>
+<script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>   
+<script src="{{ asset('front/js/docSlider.min.js') }}"></script>
+<!-- <script>docSlider.init({});</script> -->
+<script async defer src="https://buttons.github.io/buttons.js"></script>
+
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-PVMMQPFTW8"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-PVMMQPFTW8');
+</script>
+
